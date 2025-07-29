66445fb432afe675570961247175b8a84bad6ade
Ritesh <riteshkhore@gmail.com>
2025-07-29 20:34:32 +0530

    Initial Commit

---
+++ LIGames/LIGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIGamesApp.swift
+//  LIGames
+//
+//  Created by Ritesh Khore on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct LIGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
